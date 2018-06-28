d1e2e8ca5981b806c3c24d001ccd5c59ab6599c8
willsinn <30275717+willsinn@users.noreply.github.com>
2018-06-28 16:31:41 -0400

    added react-sticky

--- src/App.js
+++ src/App.js
@@ -3,34 +3,35 @@ import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './Components/Main';
 import { Link } from 'react-router-dom';
+import { StickyContainer, Sticky } from 'react-sticky';
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color:'white'}} to="/"> William Sinn </Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color:'black'}} to="/"> William Sinn </Link>}>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main />
-        </Content>
-    </Layout>
-</div>
+          <Layout>
+              <Header className="header-color" title={<Link style={{textDecoration: 'none', color:'white'}} to="/"> William Sinn </Link>} scroll>
+                  <Navigation>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Header>
+              <Drawer title={<Link style={{textDecoration: 'none', color:'black'}} to="/"> William Sinn </Link>}>
+                  <Navigation>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+                  <Main />
+              </Content>
+          </Layout>
+       </div>
     );
   }
 }
